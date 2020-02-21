eef0c0b6a47e7aedc4c05f084796ac07c48a7f10
Gongshijie <2994118178@qq.com>
2020-02-21 11:22:19 +0800

    更新viewModel创建方式

--- app/src/main/java/com/example/navdrawer/ui/send/SendFragment.java
+++ app/src/main/java/com/example/navdrawer/ui/send/SendFragment.java
@@ -6,11 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.navdrawer.R;
 
@@ -21,7 +21,7 @@ public class SendFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         sendViewModel =
-                ViewModelProviders.of(this).get(SendViewModel.class);
+                new ViewModelProvider(requireActivity()).get(SendViewModel.class);
         View root = inflater.inflate(R.layout.fragment_send, container, false);
         final TextView textView = root.findViewById(R.id.text_send);
         sendViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
